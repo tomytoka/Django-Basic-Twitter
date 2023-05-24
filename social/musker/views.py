7778a88cfcf2e88ed6644618c0ef6b2578d738aa
tomas tokatlian <tomytokatlian@gmail.com>
2023-05-23 22:21:10 -0300

    initial commit

---
+++ social/musker/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import redirect, render
+from django.contrib import messages
+from .models import Profile
+
+def home(request):
+    return render(request,'home.html',)
+
+def profile_list(request):
+    if request.user.is_authenticated: 
+        profiles = Profile.objects.exclude(user=request.user)
+        return render(request, 'profile_list.html',{"profiles":profiles})
+    else:
+        messages.success(request,("You must be logged."))
+        return redirect('home')
